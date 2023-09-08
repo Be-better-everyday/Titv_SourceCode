d05c0a1764a958cceaca5ca0f80525c691675cde
Be-better-everyday <125204550+Be-better-everyday@users.noreply.github.com>
2023-09-08 16:34:08 +0700

    13

--- 13_2(42)securityjpa_register/securityjpa/src/main/java/vn/titv/spring/securityjpa/controller/HomeController.java
+++ 13_2(42)securityjpa_register/securityjpa/src/main/java/vn/titv/spring/securityjpa/controller/HomeController.java
@@ -1,4 +1,4 @@
-package vn.titv.spring.mvcsecurity.controller;
+package vn.titv.spring.securityjpa.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/letgo")
+@RequestMapping()
 public class HomeController {
 
     @GetMapping()
